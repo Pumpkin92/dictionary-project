71b3884fa3824141a3a727b29c08e63da38b3b70
Pumpkin92 <77683675+Pumpkin92@users.noreply.github.com>
2022-01-03 17:34:50 +0000

    created synonym component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -13,6 +14,8 @@ export default function Meaning(props) {
               • {definition.definition}
               <br />
               <em>{definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
